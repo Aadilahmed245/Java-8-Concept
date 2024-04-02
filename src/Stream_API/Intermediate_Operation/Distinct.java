8fd24333e87a7bdcb34e33a3e1978080ed1f9802
Adil Qureshi <aadilahmed245@gmail.com>
2024-04-02 11:59:29 +0530

    Java 8 concept

--- src/Stream_API/Intermediate_Operation/Distinct.java
+++ src/Stream_API/Intermediate_Operation/Distinct.java
@@ -1,5 +1,10 @@
 package Stream_API.Intermediate_Operation;
 
-public class Distinct {
+import java.util.List;
 
+public class Distinct {
+    public static void main(String[] args) {
+        List<Integer> list = List.of(1,2,2,3,1,3,5,4,4,5,6);
+        list.stream().distinct().forEach(System.out::println);
+    }
 }
